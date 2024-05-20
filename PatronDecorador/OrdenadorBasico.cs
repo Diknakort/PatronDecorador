b7c4a070a1304ad5cf51fcd0de843ab03d251138
Diknakort <diknakort@gmail.com>
2024-05-20 14:04:05 +0200

    Agregar archivos de proyecto.

---
+++ PatronDecorador/OrdenadorBasico.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronDecorador
+{
+    public interface IOrdenador
+    {
+        public int precio { get; set; }
+        public int ram { get; set; }
+        public int almacenamiento { get; set; }
+    }
+    public class OrdenadorBasico : IOrdenador
+    {
+        public int precio { get; set; } = 100;
+        public int ram { get; set; } = 10;
+        public int almacenamiento { get; set; } = 100;
+
+        public override string? ToString()
+        {
+            return ($"El ordenador se compone de un precio {precio}, RAM de {ram} y con almacenamiento {almacenamiento}");
+        }
+    }
+    public class DecoradorRAMMaxima : IOrdenador
+    {
+        IOrdenador ObjetoADecorar;
+        public DecoradorRAMMaxima(IOrdenador ordenador)
+        {
+            this.ObjetoADecorar = ordenador;
+        }
+
+        public int precio { get { return this.ObjetoADecorar.precio + 200; } set { this.ObjetoADecorar.precio = value; } }
+        public int ram { get; set; } = 100;
+        public int almacenamiento { get { return this.ObjetoADecorar.almacenamiento; } set { } }
+        public override string? ToString()
+        {
+            return ($"El ordenador se compone de un precio {precio}, RAM de {ram} y con almacenamiento {almacenamiento}");
+        }
+    }
+    public class DecoradorDiscoSolido : IOrdenador
+    {
+        IOrdenador ObjetoADecorar;
+        public DecoradorDiscoSolido(IOrdenador ordenador)
+        {
+            this.ObjetoADecorar = ordenador;
+        }
+
+        public int precio { get { return this.ObjetoADecorar.precio + 100; } set { } }
+        public int ram { get { return this.ObjetoADecorar.ram; } set { } }
+        public int almacenamiento { get { return this.ObjetoADecorar.almacenamiento *2; } set { } }
+        public override string? ToString()
+        {
+            return ($"El ordenador se compone de un precio {precio}, RAM de {ram} y con almacenamiento {almacenamiento}");
+        }
+    }
+    public class DecoradorAVerSiCuela : IOrdenador
+    {
+        IOrdenador ObjetoADecorar;
+        public DecoradorAVerSiCuela(IOrdenador ordenador)
+        {
+            this.ObjetoADecorar = ordenador;
+        }
+
+        public int precio { get { return this.ObjetoADecorar.precio * 2; } set { } }
+        public int ram { get { return this.ObjetoADecorar.ram; } set { } }
+        public int almacenamiento { get { return this.ObjetoADecorar.almacenamiento; } set { } }
+        public override string? ToString()
+        {
+            return ($"El ordenador se compone de un precio {precio}, RAM de {ram} y con almacenamiento {almacenamiento}");
+        }
+    }
+}
---
+++ PatronDecorador/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PatronDecorador;
+
+IOrdenador MiOrdenadorBasico = new OrdenadorBasico();
+IOrdenador ordenadorAna = new DecoradorRAMMaxima( MiOrdenadorBasico);
+IOrdenador ordenadorLuisa = new DecoradorDiscoSolido(new DecoradorRAMMaxima( MiOrdenadorBasico));
+IOrdenador ordenadorGeronimo = new DecoradorAVerSiCuela(new DecoradorDiscoSolido(MiOrdenadorBasico));
+Console.WriteLine($"El ordenador de Manolo es:{MiOrdenadorBasico.ToString()}");
+Console.WriteLine($"El ordenador de Ana es:{ordenadorAna.ToString()}");
+Console.WriteLine($"El ordenador de Luisa es:{ordenadorLuisa.ToString()}");
+Console.WriteLine($"El ordenador de Geronimo es:{ordenadorGeronimo.ToString()}");
